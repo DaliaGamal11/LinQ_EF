fb4767a5cd3c8a137723e5f3ae750323814da3e7
Dalia Gamal <119794418+DaliaGamal11@users.noreply.github.com>
2025-04-14 18:11:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> electronicsProducts = new List<string> { "Laptop", "Smartphone", "Tablet", "Smartwatch" };
+            List<string> furnitureProducts = new List<string> { "Table", "Chair", "Smartwatch", "Laptop", "Sofa" };
+            //Retrieve all distinct products from both lists and sort them alphabetically.
+            var result = electronicsProducts
+                .Concat(furnitureProducts)
+                .Distinct()
+                .OrderBy(Product=>Product);
+            foreach (var Product in result)
+                Console.WriteLine(Product);
+        }
+    }
+}
